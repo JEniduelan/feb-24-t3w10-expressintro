574a32910eca3620171d3ee5acd1d170e905f577
John Niduelan <jeniduelan@gmail.com>
2024-10-15 20:47:16 +1100

    fix: better naming of router exports

--- src/index.js
+++ src/index.js
@@ -9,16 +9,27 @@ const app = express();
 // .get("/", (banana, elephant) => {})
 app.get("/", (request, response) => {
 	// response.send("<h1>Hello world!</h1>");
-    response.json({
-        message:"Hello World!"
-    });
+	response.json({
+		message:"Hello world!"
+	});
 });
 
 app.post("/", (request, response) => {
-    response.json({
-        message: "POST request received!"
-    });
+	response.json({
+		message: "POST request received!"
+	});
 });
+
+// http://localhost:3000/bananas
+app.post("/bananas", (request, response) => {
+	response.json({
+		message: "POST bananas received!"
+	});
+});
+
+const PokemonController = require("./controllers/pokemonController.js");
+// localhost:3000/pokemon/
+app.use("/pokemon", PokemonController);
 
 const PORT = 3000;
 
